2f1d8f0d072f9c6964331539cd3971388821e41d
Boris <BogoradBoris@gmail.com>
2020-05-19 21:36:57 -0400

    add cosntraits

---
+++ FirstApp HW2.1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp HW2.1.1
+//
+//  Created by Boris Bogorad on 5/19/20.
+//  Copyright © 2020 Boris Bogorad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
